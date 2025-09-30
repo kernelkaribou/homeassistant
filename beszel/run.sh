ac52898271d0e1912c7bc23842ad593c1c57936c
kernelkaribou <kernelkaribou@git.com>
2025-09-30 14:00:35 -0500

    set env variables before run

--- beszel/run.sh
+++ beszel/run.sh
@@ -12,16 +12,22 @@ ARGS=()
 # Determining ingress entry for App URL and base path
 INGRESS_ENTRY=$(bashio::addon.ingress_entry)
 APP_URL=$(bashio::config 'app_url')
-[ -n "${APP_URL}" ] && ARGS+=("APP_URL=${APP_URL}${INGRESS_ENTRY}")
+if [ -n "${APP_URL}" ]; then
+    export APP_URL="${APP_URL}${INGRESS_ENTRY}"
+fi
 sed -i "s#%%ingress_entry%%#${INGRESS_ENTRY}#g" /etc/nginx/servers/ingress.conf
 
 # Enable auto login if provided
 AUTO_LOGIN=$(bashio::config 'auto_login')
-[ -n "${AUTO_LOGIN}" ] && ARGS+=("AUTO_LOGIN=${AUTO_LOGIN}")
+if [ -n "${AUTO_LOGIN}" ]; then
+    export AUTO_LOGIN="${AUTO_LOGIN}"
+fi
 
 # Add disable auth if requested
 DISABLE_AUTH=$(bashio::config 'disable_auth')
-[ "${DISABLE_AUTH}" = "true" ] && ARGS+=("DISABLE_PASSWORD_AUTH=true")
+if [ "${DISABLE_AUTH}" = "true" ]; then
+    export DISABLE_PASSWORD_AUTH="true"
+fi
 
 #Ingress config debug
 cat /etc/nginx/servers/ingress.conf
@@ -34,5 +40,10 @@ bashio::log.info "Starting NGINX..."
 nginx &
 
 # Start Beszel
-bashio::log.info "Starting Beszel with args: ${ARGS[*]}"
-exec "${ARGS[@]}" /usr/local/bin/beszel
+bashio::log.info "Starting Beszel..."
+bashio::log.info "Environment variables set:"
+[ -n "${APP_URL}" ] && bashio::log.info "  APP_URL=${APP_URL}"
+[ -n "${AUTO_LOGIN}" ] && bashio::log.info "  AUTO_LOGIN=${AUTO_LOGIN}"
+[ -n "${DISABLE_PASSWORD_AUTH}" ] && bashio::log.info "  DISABLE_PASSWORD_AUTH=${DISABLE_PASSWORD_AUTH}"
+
+exec /usr/local/bin/beszel
